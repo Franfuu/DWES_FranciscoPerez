1ce9af592fb9cc420d5f18e8f547c33317b98cdd
Franfuu <franperezruiz10@gmail.com>
2026-02-04 23:30:00 +0100

    ultimos cambios añadidos al readme apicity

---
+++ UD7/2.Custom/apicity/app/Models/City.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class City extends Model
+{
+    use HasFactory;
+
+    protected $guarded = []; // Permite la asignación masiva para todos los campos
+}
